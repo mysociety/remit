50888809b3dc075ff7c8c5c26f4815406d488428
Steven Day <steve@mysociety.org>
2015-12-17 17:28:17 +0000

    Add Vagrantfile

---
+++ Vagrantfile
@@ -0,0 +1,43 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  # Every Vagrant virtual environment requires a box to build off of.
+  config.vm.box = "precise64"
+
+  # The url from where the 'config.vm.box' box will be fetched if it
+  # doesn't already exist on the user's system.
+  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+
+  # Enable NFS access to the disk
+  config.vm.synced_folder "", "/vagrant", :nfs => true
+
+  # NFS requires a host-only network
+  config.vm.network :private_network, ip: "10.11.12.13"
+
+  # Forward ports too so that you can access the vm in your local network
+  # through your hosts' IP address
+
+  # For the Rails server
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+
+  # For Mailcatcher
+  config.vm.network "forwarded_port", guest: 1080, host: 1080
+
+  # For Elasticsearch
+  config.vm.network "forwarded_port", guest: 9200, host: 9200
+
+  # provisioning
+  config.vm.provision "shell", path: "script/provision.sh", privileged: false
+
+  config.ssh.forward_agent = true
+
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 2048
+    v.cpus = 2
+  end
+
+end
---
+++ script/provision.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+export LANGUAGE="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
+# We need a new repo to get elasticsearch
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb http://packages.elastic.co/elasticsearch/0.90/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-0.90.list
+
+sudo apt-get update
+sudo apt-get install -y curl git openjdk-6-jre libpq-dev postgresql nodejs
+sudo apt-get install elasticsearch=0.90.3
+
+# Make elastic search run at startup
+sudo update-rc.d elasticsearch defaults
+
+#install rvm
+# TODO: Needs Jones truncation armour, currently fails if used: http://drj11.wordpress.com/2014/03/19/piping-into-shell-may-be-harmful/
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+source .bash_profile
+
+rvm install 1.9.3-p551
+
+cd /vagrant
+
+# cp config/carrierwave.yml-example config/carrierwave.yml
+# cp config/exception_notification.yml-example config/exception_notification.yml
+cp config/database.yml-example config/database.yml
+cp config/secrets.yml-example config/secrets.yml
+cp config/general.yml-example config/general.yml
+
+bundle install
+
+echo "ReMIT installed successfully!"
+echo "To start the server: vagrant ssh, cd /vagrant, rails s -b 0.0.0.0, the server will then run at http://localhost:3000/"
