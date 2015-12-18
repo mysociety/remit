cfe9e1230fda0015898643d6e614b23d758461f8
Steven Day <steve@mysociety.org>
2015-12-18 10:37:51 +0000

    Add a very basic homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "example" do
+RSpec.describe "Homepage" do
   it "exists" do
-    expect(1 + 1).to eq 2
+    visit "/"
+    expect(page).to have_text("ReMIT")
   end
 end
