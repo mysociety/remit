75a4b806bb1513c3cf2d31233c6fb1b95ad916f0
vagrant <vagrant@precise64.(none)>
2016-01-05 11:47:19 +0000

    Add initial data model

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "home/index.html.erb", type: :view do
   it "should print the name of the project" do
     render
-    expect(rendered).to match /ReMIT/
+    expect(rendered).to match(/ReMIT/)
   end
 end
