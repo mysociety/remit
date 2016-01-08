0df3a5cff5ca4ece48a92603b8ff2107c82fb358
Steven Day <steve@mysociety.org>
2016-01-08 14:40:37 +0000

    Tidy up the admin menu a little bit

--- app/admin/impact_type.rb
+++ app/admin/impact_type.rb
@@ -1,3 +1,4 @@
 ActiveAdmin.register ImpactType do
   permit_params :name, :description
+  menu parent: "Field options"
 end
--- app/admin/study_type.rb
+++ app/admin/study_type.rb
@@ -1,3 +1,4 @@
 ActiveAdmin.register StudyType do
   permit_params :name, :description
+  menu parent: "Field options"
 end
