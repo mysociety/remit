82feeef9272bf25677992db76c0a0f6eeec332ef
Steven Day <steve@mysociety.org>
2016-01-12 17:48:18 +0000

    Fix some scss/ruby linting issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'study' => 'home#study'
+  get "study" => "home#study"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
