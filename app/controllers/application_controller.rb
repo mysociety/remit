668f0f9d54acc75a61b9138f869320dc36d382f7
Steven Day <steve@mysociety.org>
2016-01-14 12:55:17 +0000

    Add name field to user signup/account editing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # Configure Devise's permitted params
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Authenticate the currently logged in user for access to the the admin
   # site. ActiveAdmin calls this
@@ -24,5 +26,12 @@ class ApplicationController < ActionController::Base
       else
         root_path
       end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 end
