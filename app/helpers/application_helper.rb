bf2be66fcb8950490f56e84322d3922202d00590
Steven Day <steve@mysociety.org>
2016-01-22 16:15:29 +0000

    Add a helper to produce a timeline for study stage changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,75 @@
 module ApplicationHelper
+  # Return a hash that details a study's progress through the stages
+  # Useful for printing the progress indicator on study pages
+  def study_timeline(study)
+    # Initialise a timeline object with a nice label for each stage
+    # and a state field for whether it's "done", "doing" or not yet reached.
+    stages = Study.study_stages.keys.map(&:to_sym)
+    timeline = initial_study_timeline(stages)
+
+    # We only want to show the "withdrawn" stage if the study is actually
+    # withdrawn though...
+    unless study.withdrawn_postponed?
+      timeline.delete(:withdrawn_postponed)
+    end
+
+    # Fill in the state field based on the activities we've recorded against
+    # the study.
+    stage_changes = study.activities.
+                    where(key: "study.study_stage_changed").
+                    order(created_at: :asc)
+    timeline = populate_timeline_states(timeline, stage_changes)
+
+    # Ensure that stages before the current one are completed, even if the
+    # history shows the study jumped straight to it (e.g. because we
+    # imported it when it was already in progress, or completed).
+    # Only for non-withdrawn studies, because we can't tell what other
+    # stages a withdrawn study would have been through otherwise.
+    current_stage_index = stages.index(study.study_stage.to_sym)
+    unless study.withdrawn_postponed?
+      timeline = ensure_timeline_is_complete timeline,
+                                             stages,
+                                             current_stage_index
+    end
+
+    # Finally, ensure that the current stage is marked as "doing"
+    timeline[stages[current_stage_index]][:state] = "doing"
+
+    timeline
+  end
+
+  protected
+
+  def initial_study_timeline(stages)
+    timeline = {}
+    stages.each do |stage|
+      timeline[stage] = {
+        label: Study::STUDY_STAGE_LABELS[stage],
+        state: ""
+      }
+    end
+    timeline
+  end
+
+  def populate_timeline_states(timeline, changes)
+    changes.each do |change_activity|
+      stage_before = change_activity.parameters[:before].to_sym
+      stage_after = change_activity.parameters[:after].to_sym
+      unless stage_before.blank?
+        timeline[stage_before][:state] = "done"
+      end
+      timeline[stage_after][:state] = "doing"
+    end
+    timeline
+  end
+
+  def ensure_timeline_is_complete(timeline, stages, current_stage_index)
+    if current_stage_index > 0
+      stages_before = stages.slice(0..current_stage_index - 1)
+      stages_before.each do |stage|
+        timeline[stage][:state] = "done"
+      end
+    end
+    timeline
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,175 @@
+require "rails_helper"
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe "#study_timeline" do
+    let(:base_timeline) do
+      {
+        concept: { label: "Concept", state: "" },
+        protocol_erb: { label: "Protocol & ERB", state: "" },
+        delivery: { label: "Delivery", state: "" },
+        output: { label: "Output", state: "" },
+        completion: { label: "Completion", state: "" },
+      }
+    end
+
+    context "given a study with no history" do
+      context "when the study is in the concept stage" do
+        let(:study) { FactoryGirl.create(:study) }
+
+        it "returns a timeline with one entry" do
+          expected_timeline = base_timeline
+          expected_timeline[:concept][:state] = "doing"
+          expect(study_timeline(study)).to eq expected_timeline
+        end
+      end
+
+      context "when the study is in the protocol_erb stage" do
+        let(:study) { FactoryGirl.create(:study, study_stage: "protocol_erb") }
+
+        it "returns a timeline with multiple entries" do
+          expected_timeline = base_timeline
+          expected_timeline[:concept][:state] = "done"
+          expected_timeline[:protocol_erb][:state] = "doing"
+          expect(study_timeline(study)).to eq expected_timeline
+        end
+      end
+
+      context "when the study is in the completion stage" do
+        let(:study) { FactoryGirl.create(:study, study_stage: "completion") }
+
+        it "returns a timeline with multiple entries" do
+          expected_timeline = base_timeline
+          expected_timeline[:concept][:state] = "done"
+          expected_timeline[:protocol_erb][:state] = "done"
+          expected_timeline[:delivery][:state] = "done"
+          expected_timeline[:output][:state] = "done"
+          expected_timeline[:completion][:state] = "doing"
+          expect(study_timeline(study)).to eq expected_timeline
+        end
+      end
+
+      context "when the study is in the withdrawn stage" do
+        let(:study) do
+          FactoryGirl.create(:study, study_stage: "withdrawn_postponed")
+        end
+
+        it "returns a timeline with one entry" do
+          expect(study.withdrawn_postponed?).to be true
+          expected_timeline = base_timeline
+          expected_timeline[:withdrawn_postponed] = {
+            label: "Withdrawn or Postponed",
+            state: "doing"
+          }
+          expect(study_timeline(study)).to eq expected_timeline
+        end
+      end
+    end
+
+    context "given a study with history" do
+      let(:study) { FactoryGirl.create(:study) }
+
+      before do
+        PublicActivity.enabled = true
+      end
+
+      after do
+        PublicActivity.enabled = false
+      end
+
+      context "when the study is in the concept stage" do
+        let(:study) { FactoryGirl.create(:study) }
+
+        it "returns a timeline with one entry" do
+          expected_timeline = base_timeline
+          expected_timeline[:concept][:state] = "doing"
+          expect(study_timeline(study)).to eq expected_timeline
+        end
+      end
+
+      context "when the study is in the protocol_erb stage" do
+        let(:study) { FactoryGirl.create(:study) }
+
+        it "returns a timeline with multiple entries" do
+          study.study_stage = "protocol_erb"
+          study.save!
+
+          expected_timeline = base_timeline
+          expected_timeline[:concept][:state] = "done"
+          expected_timeline[:protocol_erb][:state] = "doing"
+          expect(study_timeline(study)).to eq expected_timeline
+        end
+      end
+
+      context "when the study is in the completion stage" do
+        let(:study) { FactoryGirl.create(:study) }
+
+        it "returns a timeline with multiple entries" do
+          study.study_stage = "protocol_erb"
+          study.save!
+          study.study_stage = "delivery"
+          study.save!
+          study.study_stage = "output"
+          study.save!
+          study.study_stage = "completion"
+          study.save!
+
+          expected_timeline = base_timeline
+          expected_timeline[:concept][:state] = "done"
+          expected_timeline[:protocol_erb][:state] = "done"
+          expected_timeline[:delivery][:state] = "done"
+          expected_timeline[:output][:state] = "done"
+          expected_timeline[:completion][:state] = "doing"
+          expect(study_timeline(study)).to eq expected_timeline
+        end
+      end
+
+      context "when a study has skipped a stage" do
+        let(:study) { FactoryGirl.create(:study) }
+
+        it "returns a timeline with multiple entries" do
+          study.study_stage = "protocol_erb"
+          study.save!
+          study.study_stage = "delivery"
+          study.save!
+          study.study_stage = "completion"
+          study.save!
+
+          expected_timeline = base_timeline
+          expected_timeline[:concept][:state] = "done"
+          expected_timeline[:protocol_erb][:state] = "done"
+          expected_timeline[:delivery][:state] = "done"
+          # We expect the helper to fill this in for us anyway
+          expected_timeline[:output][:state] = "done"
+          expected_timeline[:completion][:state] = "doing"
+          expect(study_timeline(study)).to eq expected_timeline
+        end
+      end
+
+      context "when a study is in the withdrawn stage" do
+        let(:study) { FactoryGirl.create(:study) }
+
+        it "returns a timeline with entries only for completed stages" do
+          study.study_stage = "protocol_erb"
+          study.save!
+          study.study_stage = "delivery"
+          study.save!
+          study.study_stage = "withdrawn_postponed"
+          study.save!
+
+          expected_timeline = base_timeline
+          expected_timeline[:concept][:state] = "done"
+          expected_timeline[:protocol_erb][:state] = "done"
+          expected_timeline[:delivery][:state] = "done"
+          # We expect the helper to leave these two alone
+          expected_timeline[:output][:state] = ""
+          expected_timeline[:completion][:state] = ""
+          expected_timeline[:withdrawn_postponed] = {
+            label: "Withdrawn or Postponed",
+            state: "doing"
+          }
+          expect(study_timeline(study)).to eq expected_timeline
+        end
+      end
+    end
+  end
+end
