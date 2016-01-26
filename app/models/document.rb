1ae45097e23f386267f5fccfb0911736bf2be41a
Steven Day <steve@mysociety.org>
2016-01-26 17:50:19 +0000

    Add document field to Document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,11 +2,15 @@
 #
 # Table name: documents
 #
-#  id               :integer          not null, primary key
-#  document_type_id :integer          not null
-#  study_id         :integer          not null
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
+#  id                    :integer          not null, primary key
+#  document_type_id      :integer          not null
+#  study_id              :integer          not null
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  document_file_name    :string
+#  document_content_type :string
+#  document_file_size    :integer
+#  document_updated_at   :datetime
 #
 # Indexes
 #
@@ -16,9 +20,22 @@
 
 class Document < ActiveRecord::Base
   include StudyActivityTrackable
+  ALLOWED_CONTENT_TYPES = [
+    "application/pdf",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.ms-excel",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/vnd.ms-powerpoint",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+  ].freeze
 
   belongs_to :document_type, inverse_of: :documents
   belongs_to :study, inverse_of: :documents
   validates :document_type, presence: true
   validates :study, presence: true
+
+  has_attached_file :document
+  validates_attachment :document,
+                       content_type: { content_type: ALLOWED_CONTENT_TYPES }
 end
