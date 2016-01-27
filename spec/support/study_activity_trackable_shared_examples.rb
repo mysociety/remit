5084d65b8347ab9fbffe54fafc60ac3771fab9bc
Steven Day <steve@mysociety.org>
2016-01-27 13:41:25 +0000

    Make sure activities aren't created until the related model is

--- spec/support/study_activity_trackable_shared_examples.rb
+++ spec/support/study_activity_trackable_shared_examples.rb
@@ -13,30 +13,34 @@ RSpec.shared_examples_for "study_activity_trackable" do
   end
 
   it "logs an activity on the study when created" do
-    study = FactoryGirl.create(:study)
-    resource = FactoryGirl.create(model, study: study)
-    study.reload
-    expect(study.activities.length).to eq 2
-    expect(study).to have_latest_activity("study.#{model}_added", {})
-    expect(study.activities.first.related_content).to eq resource
+    TestAfterCommit.with_commits(true) do
+      study = FactoryGirl.create(:study)
+      resource = FactoryGirl.create(model, study: study)
+      study.reload
+      expect(study.activities.length).to eq 2
+      expect(study).to have_latest_activity("study.#{model}_added", {})
+      expect(study.activities.first.related_content).to eq resource
+    end
   end
 
   it "logs an activity on the study if created via the study's association" do
-    study = FactoryGirl.create(:study)
-    association = study.send model.to_s.pluralize.to_sym
-    # See https://github.com/thoughtbot/factory_girl/issues/359
-    # We want to use FactoryGirl to get the attributes in a generic way, so
-    # that we can then create a resource using the Study's association's
-    # create method (e.g. study.documents.create(attrs)). But the usual
-    # attributes_for will ignore any associations on the factory, so models
-    # which need some other things building won't be valid.
-    attrs = FactoryGirl.build(model).attributes.delete_if do |k, _v|
-      %w(id study_id created_at updated_at).member?(k)
+    TestAfterCommit.with_commits(true) do
+      study = FactoryGirl.create(:study)
+      association = study.send model.to_s.pluralize.to_sym
+      # See https://github.com/thoughtbot/factory_girl/issues/359
+      # We want to use FactoryGirl to get the attributes in a generic way, so
+      # that we can then create a resource using the Study's association's
+      # create method (e.g. study.documents.create(attrs)). But the usual
+      # attributes_for will ignore any associations on the factory, so models
+      # which need some other things building won't be valid.
+      attrs = FactoryGirl.build(model).attributes.delete_if do |k, _v|
+        %w(id study_id created_at updated_at).member?(k)
+      end
+      resource = association.create!(attrs)
+      study.reload
+      expect(study.activities.length).to eq 2
+      expect(study).to have_latest_activity("study.#{model}_added", {})
+      expect(study.activities.first.related_content).to eq resource
     end
-    resource = association.create!(attrs)
-    study.reload
-    expect(study.activities.length).to eq 2
-    expect(study).to have_latest_activity("study.#{model}_added", {})
-    expect(study.activities.first.related_content).to eq resource
   end
 end
