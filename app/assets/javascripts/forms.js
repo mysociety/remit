1cbf65ed4743076e7a1598ae0c732d04c793a82f
Zarino Zappia <mail@zarino.co.uk>
2016-02-01 13:57:13 +0000

    Reveal secondary text input when "Other" dissemination category is selected
    Part of #3.

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -10,4 +10,32 @@ $(function(){
 
         $(this).selectpicker(settings);
     });
+
+    // The dissemination category dropdown has two "Other" options
+    // that should reveal a secondary (optional) text input for you
+    // to give a short name for the category.
+    var showOrHideOtherCategoryInput = function showOrHideOtherCategoryInput(){
+        var $select = $('#dissemination_dissemination_category_id')
+        var $selectedOption = $select.find('option[value="' + $select.val() + '"]');
+        var isOtherCategory = $selectedOption.is('[data-other-category]');
+
+        if(isOtherCategory){
+            $('.dissemination_other_category').slideDown(100);
+        } else {
+            $('.dissemination_other_category').slideUp(100);
+        }
+    }
+
+    // Trigger the check on pageload, just in case.
+    showOrHideOtherCategoryInput();
+
+    // And then trigger it when the category dropdown is changed.
+    $('#dissemination_dissemination_category_id').on('change', function(e){
+        showOrHideOtherCategoryInput();
+    });
+
+    // Hide the other_category input, and give it a better label
+    // now that it only ever gets shown by JavaScript.
+    $('.dissemination_other_category').hide()
+        .find('label').text('Describe the category in a couple of words');
 });
