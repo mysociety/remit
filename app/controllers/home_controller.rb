ea6035bfe0dda483ec4dd7daa3fe95a9132a314c
Steven Day <steve@mysociety.org>
2016-02-04 15:42:52 +0000

    Wire up count of studies being listed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @studies = Study.order(updated_at: :desc).page(params[:page]).per(10)
+    @total_study_count = Study.count
   end
 end
