b50de9d5fb0a43c8258f6a25b6461f9531fb021e
Steven Day <steve@mysociety.org>
2016-02-05 14:49:51 +0000

    Update factories to avoid database uniqueness errors

--- spec/factories/document.rb
+++ spec/factories/document.rb
@@ -7,5 +7,12 @@ FactoryGirl.define do
       fixture_file_upload(Rails.root.join("spec", "fixtures", "test.pdf"),
                           "application/pdf")
     end
+
+    after(:build) do |d|
+      if d.document_type.nil?
+        protocol_doc_type = DocumentType.find_by_name("Protocol")
+        d.document_type = protocol_doc_type || create(:protocol_doc_type)
+      end
+    end
   end
 end
