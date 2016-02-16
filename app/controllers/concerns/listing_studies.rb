f5178029a15110de59dfa848170a0b28537cdff9
Dave Arter <davea@mysociety.org>
2016-02-16 14:55:38 +0000

    Enable filtering by study type; populate methodology filter drop-down

--- app/controllers/concerns/listing_studies.rb
+++ app/controllers/concerns/listing_studies.rb
@@ -4,6 +4,7 @@ module ListingStudies
   included do
     before_action :set_include_archived, only: :index
     before_action :set_study_scope, only: :index
+    before_action :set_filter_form_values, only: :index
   end
 
   def set_include_archived
@@ -15,5 +16,23 @@ module ListingStudies
     if @include_archived
       @study_scope = :not_withdrawn
     end
+  end
+
+  def set_filter_form_values
+    @study_types = StudyType.all.order(name: :asc)
+  end
+
+  def get_filtered_studies
+    studies = Study.send(@study_scope)
+
+    unless params[:pi].blank?
+      studies = studies.joins(:principal_investigator).where('lower("users"."name") = ?', params[:pi].downcase)
+    end
+
+    unless params[:study_type].blank?
+      studies = studies.joins(:study_type).where('lower("study_types"."name") = ?', params[:study_type].downcase)
+    end
+
+    studies
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,14 +2,8 @@ class SearchController < ApplicationController
   include ListingStudies
 
   def index
-    studies = Study.send(@study_scope)
-
-    unless params[:pi].blank?
-      studies = studies.joins(:principal_investigator).where('lower(users.name) = ?', params[:pi].downcase)
-    end
-
     # rubocop:disable Style/MultilineOperationIndentation
-    @studies = studies.
+    @studies = get_filtered_studies.
                  order(updated_at: :desc).
                  page(params[:page]).
                  per(10)
