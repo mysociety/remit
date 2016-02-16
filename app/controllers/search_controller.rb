0d19d78b7f895dbfab0efb8c99e482a602f92326
Dave Arter <davea@mysociety.org>
2016-02-16 12:56:43 +0000

    Enable filtering by PI on search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,17 @@ class SearchController < ApplicationController
   include ListingStudies
 
   def index
+    studies = Study.send(@study_scope)
+
+    unless params[:pi].blank?
+      studies = studies.joins(:principal_investigator).where('lower(users.name) = ?', params[:pi].downcase)
+    end
+
     # rubocop:disable Style/MultilineOperationIndentation
-    @studies = Study.send(@study_scope).
-                     order(updated_at: :desc).
-                     page(params[:page]).
-                     per(10)
+    @studies = studies.
+                 order(updated_at: :desc).
+                 page(params[:page]).
+                 per(10)
     # rubocop:enable Style/MultilineOperationIndentation
   end
 end
