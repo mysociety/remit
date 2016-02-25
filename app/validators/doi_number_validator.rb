1f088a284bf181910f5c881a7cb125e1bb3e3e19
Steven Day <steve@mysociety.org>
2016-02-25 11:38:25 +0000

    Rework the DOI lookup to talk to cross-ref directly. Closes #136

--- app/validators/doi_number_validator.rb
+++ app/validators/doi_number_validator.rb
@@ -1,13 +1,10 @@
-require "biburi"
+require "net/http"
+require "json"
 
 class DoiNumberValidator < ActiveModel::Validator
   def validate(record)
     return if record.doi_number.blank?
-    begin
-      result = BibURI.lookup(record.doi_number)
-    rescue
-      result = nil
-    end
+    result = lookup_doi(record.doi_number)
     if result.blank?
       message = "we couldn't find that DOI number, can you check it " \
                 "for typos? If everything's correct you might have to put " \
@@ -15,10 +12,10 @@ class DoiNumberValidator < ActiveModel::Validator
       record.errors.add(:doi_number, message)
       return
     end
-    record.article_title = result.title.to_s
-    record.lead_author = result.author.to_s
-    record.book_or_journal_title = result.journal.to_s
-    record.publication_year = result.year.to_s
+    record.article_title = result[:article_title]
+    record.lead_author = result[:lead_author]
+    record.book_or_journal_title = result[:book_or_journal_title]
+    record.publication_year = result[:publication_year]
     if record.article_title.blank? ||
        record.lead_author.blank? ||
        record.book_or_journal_title.blank? ||
@@ -26,6 +23,44 @@ class DoiNumberValidator < ActiveModel::Validator
       message = "it doesn't look like we could get everything we " \
                 "need from the DOI number. Can you fill in the missing pieces?"
       record.errors.add(:doi_number, message)
+    end
+  end
+
+  def lookup_doi(doi)
+    url = "http://api.crossref.org/works/#{doi}"
+    uri = URI(url)
+    begin
+      response = Net::HTTP.get(uri)
+      result = JSON.parse(response)["message"]
+      title = nil
+      if result["title"].present?
+        title = result["title"].first
+      end
+      journal = nil
+      if result["publisher"].present?
+        journal = result["publisher"]
+      end
+      author = nil
+      if result["author"].present?
+        result_author = result["author"].first
+        if result_author.present?
+          author = "#{result_author['family']}, #{result_author['given']}"
+        end
+      end
+      year = nil
+      if result["issued"].present? &&
+         result["issued"]["date-parts"].present?
+        year = result["issued"]["date-parts"].first.first
+      end
+      return {
+        book_or_journal_title: journal,
+        article_title: title,
+        lead_author: author,
+        publication_year: year
+      }
+    rescue
+      # Return nothing
+      Rails.logger.error($!.backtrace)
     end
   end
 end
