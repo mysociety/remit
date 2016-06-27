ea3306927acbe6992065812308d7f70a7623e172
Zarino Zappia <mail@zarino.co.uk>
2016-06-27 14:59:29 +0100

    Allow only one output type to be recorded at a time
    Entering text into any of the textareas or text inputs inside the
    outputs recording form automatically disables all but the currently
    active form section.
    
    Clearing the entered text (or, additionally, in the case of the "other
    impacts" section, using the checkboxes to hide a previously edited
    textarea) resets the disabled sections, allowing you to change your
    mind and switch to recording a different output type.

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -47,47 +47,29 @@ $(function(){
     $(this).trigger('submit');
   }).children('.btn-success').hide();
 
-  // Monitor form elements inside the outputs sections and mark each section
-  // as 'dirty' if the user changes anything
-  var $outputsRadios = $('.output-type-accordion input[type="radio"]');
-  // The houdini css works on the next sibling element with a class
-  // but it might be immediately adjacent or come after a label for
-  // the input we've just selected as 'radio', hence .nextAll()
-  $outputsRadios.nextAll('.houdini-target').find(':input').on('change', function() {
-    $(this).parents('.houdini-target').addClass('dirty');
-    // TODO - what if they delete everything? Ideally we'd reset dirty here,
-    // but it's an edge case and they can just click 'ok' to continue anyway
-  });
+  // Checks the active output type section for altered inputs,
+  // and if found, disables the other sections, so you can't submit
+  // data in multiple sections at the same time.
+  var updateOutputTypeAccordion = function updateOutputTypeAccordion(){
+    var $section = $('.output-type-input:checked').next().next(); // get the 'houdini-target'
+    var $inputs = $section.find('textarea:visible, input[type="text"]:visible');
+    var sectionIsDirty = false;
+    $inputs.each(function(){
+      if( $.trim($(this).val()) !== '' ){
+        sectionIsDirty = true;
+      }
+    });
+    if(sectionIsDirty){
+      $section.siblings('.action-sheet__section').each(function(){
+        $(this).prev().prev().prop('disabled', true);
+      });
+    } else {
+      $section.siblings('.action-sheet__section').each(function(){
+        $(this).prev().prev().removeProp('disabled');
+      });
+    }
+  }
 
-  // Warn people when they fill in one form on the outputs page, and then
-  // select another one (because only one will be saved).
-  var $currOutput = $outputsRadios.filter(':checked');
-  var firstOutputChangeEvent = true;
-  $outputsRadios.on('change', function() {
-    // If nothing's selected when the page loads, $currOutput will be empty.
-    // So, our code won't know where to 'go back' to if the user says cancel
-    // in the dialog. This sets that up, for the first click only (after that
-    // the code below can handle things).
-    if (firstOutputChangeEvent) {
-      firstOutputChangeEvent = false;
-      if ($currOutput.length === 0) {
-        $currOutput = $(this);
-      }
-    }
-    // We only get tiggered after something is checked, so test all the
-    // unchecked radios' forms to see if they're dirty
-    if ($outputsRadios.not(':checked').nextAll('.dirty').length > 0) {
-      var sectionName = $('label[for="' + $currOutput.attr('id') + '"] .output-type-label__title').text();
-      var msg = 'Selecting this option will lose the data you\'ve already entered in the ' + sectionName + ' section.\n\nAre you sure you want to change?';
-      if (window.confirm(msg)) {
-        // Allow the switch to a new checked radio
-        $currOutput = $(this);
-      } else {
-        // Reset the checked radio to the previous one
-        console.log('Going back');
-        $(this).prop('checked', false);
-        $currOutput.prop('checked', true);
-      }
-    }
-  });
+  $('.output-type-accordion .action-sheet__section').on('keyup change', updateOutputTypeAccordion);
+
 });
