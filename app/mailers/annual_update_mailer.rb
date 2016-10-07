3565be34d7ada1e666a9aab91351df53c8931f2d
Steven Day <steve@mysociety.org>
2016-10-07 14:43:06 +0100

    Temporarily switch email sending for update invites to send to Kim

--- app/mailers/annual_update_mailer.rb
+++ app/mailers/annual_update_mailer.rb
@@ -3,6 +3,7 @@ class AnnualUpdateMailer < ApplicationMailer
     @studies = user.principal_investigator_studies.active
     @invites = invites
     @user = user
-    mail to: @user.email
+    # mail to: @user.email
+    mail to: "kim.west@london.msf.org"
   end
 end
--- app/mailers/delivery_update_invite_mailer.rb
+++ app/mailers/delivery_update_invite_mailer.rb
@@ -2,6 +2,7 @@ class DeliveryUpdateInviteMailer < ApplicationMailer
   def invite(user, invites)
     @invites = invites
     @user = user
-    mail to: @user.email
+    # mail to: @user.email
+    mail to: "kim.west@london.msf.org"
   end
 end
