a319d1c705a998ae3d289eb1c1adac046bcbfdd4
denissellu <hingasellu@gmail.com>
2017-09-15 12:15:33 +0100

    When the operating location is change update the generated reference number based on the last study in the admin

--- app/assets/javascripts/admin_reference_numbers.js
+++ app/assets/javascripts/admin_reference_numbers.js
@@ -3,14 +3,24 @@
     // Only act on the new study admin page, just in case
     if ($('body.admin_studies.new').length > 0) {
       var $generatedReferenceId = $('#study_generated_reference_id');
+
       if ($generatedReferenceId.val() === '') {
-        var prefix = window.ReMIT.currentReferenceNumberYear + '-';
-        var nextNumber = window.ReMIT.currentReferenceNumber + 1;
-        if(nextNumber < 10) {
-          nextNumber = '0' + nextNumber.toString();
-        }
-        $generatedReferenceId.val(prefix + nextNumber);
+        $generatedReferenceId.val(generate_reference(window.ReMIT.currentReferenceNumber));
       }
+
+      $("#study_operating_center").on('change', function() {
+        $generatedReferenceId.val(generate_reference($(this).children('option:selected').data("current-reference-number")));
+      });
+    }
+
+    function generate_reference(currentReferenceNumber) {
+      var prefix = window.ReMIT.currentReferenceNumberYear + '-';
+      var nextNumber = currentReferenceNumber + 1;
+      if(nextNumber < 10) {
+        nextNumber = '0' + nextNumber.toString();
+      }
+
+      return prefix + nextNumber
     }
   });
 })(window.jQuery);
